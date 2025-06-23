8dbdc3f797aaf85729aaaca014f62d9ddc2b67b1
kheangmeng <kheangmengte@gmail.com>
2025-06-23 19:00:35 +0700

    change import routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import HomePage from '@/pages/HomePage.vue'
+import AboutPage from '@/pages/AboutPage.vue'
+import LoginPage from '@/pages/LoginPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,18 +9,33 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../pages/HomePage.vue'),
+      component: HomePage,
     },
     {
       path: '/about',
       name: 'about',
-      component: () => import('../pages/AboutPage.vue'),
+      component: AboutPage,
     },
     {
       path: '/login',
       name: 'login',
-      component: () => import('../pages/LoginPage.vue'),
+      component: LoginPage,
     },
+    // {
+    //   path: '/',
+    //   name: 'home',
+    //   component: () => import('../pages/HomePage.vue'),
+    // },
+    // {
+    //   path: '/about',
+    //   name: 'about',
+    //   component: () => import('../pages/AboutPage.vue'),
+    // },
+    // {
+    //   path: '/login',
+    //   name: 'login',
+    //   component: () => import('../pages/LoginPage.vue'),
+    // },
   ],
 })
 
